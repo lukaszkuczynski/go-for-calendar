2c88be9a4f3cbf758fec2273a62d878f2b4c8ced
Lukasz Kuczynski <lukasz.kuczynski@bp.com>
2023-12-14 08:03:00 +0100

    playing with maps , slices and other gogoodies

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package go_for_calendar
 
 import (
+	"errors"
 	"fmt"
 	"time"
 )
@@ -20,4 +21,51 @@ func main() {
 	var name = "Kuczas"
 	say_hi((name))
 	fmt.Println("The time is", time.Now())
+}
+
+func aFunctionThatGetsWord() string {
+	var a string
+	a = "the_word"
+	return a
+}
+
+// Playing with loops
+func createNDigit(noOfDigits int) string {
+	var result string
+	// for i, value := range noOfDigits {
+	// 	result += fmt.Sprint(i)
+
+	// }
+	for i := 1; i < noOfDigits+1; i++ {
+		result += fmt.Sprint(i)
+	}
+	return result
+}
+
+// how to switch
+func gimmeTextOfNumber(aNumber int) string {
+	switch aNumber {
+	case 1:
+		return "one"
+	case 2:
+		return "two"
+	default:
+		return "unknown"
+	}
+}
+
+func countNoOfElementsInSlice(aSlice []int) int {
+	return len(aSlice)
+}
+
+func getACapitalCity(country string) (string, error) {
+	capitalCountryMap := make(map[string]string)
+	capitalCountryMap["Poland"] = "Warsaw"
+	capitalCountryMap["Germany"] = "Berlin"
+	capitalCountryMap["France"] = "Paris"
+	capital, ok := capitalCountryMap[country]
+	if !ok {
+		return "", errors.New("Country not found!")
+	}
+	return capital, nil
 }
--- main_test.go
+++ main_test.go
@@ -22,3 +22,44 @@ func TestFor5(t *testing.T) {
 	assert.Equal(t, expected, actual)
 
 }
+
+func TestForAFunctionThatGetsWord(t *testing.T) {
+	var actual = aFunctionThatGetsWord()
+	assert.Equal(t, actual, "the_word")
+}
+
+func TestForNDigitText(t *testing.T) {
+	var actual = createNDigit(4)
+	expected := "1234"
+	assert.Equal(t, expected, actual)
+}
+
+func TestForNumberSwitch(t *testing.T) {
+	actual := gimmeTextOfNumber(2)
+	expected := "two"
+	assert.Equal(t, actual, expected)
+}
+
+func TestForSliceSizeCounting(t *testing.T) {
+	var someSlice = make([]int, 5)
+	// someArray[3] = 1
+	// someArray[2] = 2
+	actual := countNoOfElementsInSlice(someSlice)
+	expected := 5
+	assert.Equal(t, expected, actual)
+}
+
+func TestGetRange(t *testing.T) {
+	var sliceDiv10 = [5]int{10, 20, 30, 40}
+	assert.Contains(t, sliceDiv10, 10)
+	assert.NotContains(t, sliceDiv10[1:2], 30)
+}
+
+func TestMapReturnsCorrectStuff(t *testing.T) {
+	expected := "Paris"
+	actual, _ := getACapitalCity("France")
+	assert.Equal(t, expected, actual)
+	actual2, errors2 := getACapitalCity("ThatCountryDoesntExist")
+	assert.Equal(t, "", actual2)
+	assert.NotNil(t, errors2)
+}
