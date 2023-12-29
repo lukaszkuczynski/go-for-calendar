8009928fad5f4f7f0a0cbbb77f562a5cfcef8a17
Lukasz Kuczynski <lukasz.kuczynski@bp.com>
2023-12-29 11:20:05 +0100

    logout

--- readcalendar.go
+++ readcalendar.go
@@ -3,9 +3,7 @@ package main
 import (
 	"context"
 	"encoding/gob"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -17,20 +15,6 @@ import (
 	"google.golang.org/api/calendar/v3"
 	"google.golang.org/api/option"
 )
-
-// Retrieve a token, saves the token, then returns the generated client.
-func getClient(config *oauth2.Config) *http.Client {
-	// The file token.json stores the user's access and refresh tokens, and is
-	// created automatically when the authorization flow completes for the first
-	// time.
-	tokFile := "token.json"
-	tok, err := tokenFromFile(tokFile)
-	if err != nil {
-		tok = getTokenFromWeb(config)
-		saveToken(tokFile, tok)
-	}
-	return config.Client(context.Background(), tok)
-}
 
 var (
 	googleOauthConfig *oauth2.Config
@@ -69,32 +53,10 @@ func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
 	return tok
 }
 
-// Retrieves a token from a local file.
-func tokenFromFile(file string) (*oauth2.Token, error) {
-	f, err := os.Open(file)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-	tok := &oauth2.Token{}
-	err = json.NewDecoder(f).Decode(tok)
-	return tok, err
-}
-
-// Saves a token to a file path.
-func saveToken(path string, token *oauth2.Token) {
-	fmt.Printf("Saving credential file to: %s\n", path)
-	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
-	if err != nil {
-		log.Fatalf("Unable to cache oauth token: %v", err)
-	}
-	defer f.Close()
-	json.NewEncoder(f).Encode(token)
-}
-
 func main() {
 	http.HandleFunc("/", handleMain)
 	http.HandleFunc("/login", handleGoogleLogin)
+	http.HandleFunc("/logout", handleGoogleLogout)
 	http.HandleFunc("/callback", handleGoogleCallback)
 	http.ListenAndServe(":8080", nil)
 
@@ -112,6 +74,7 @@ func handleMain(w http.ResponseWriter, r *http.Request) {
 		eventsText, err := getMyEvents(token2)
 		if err == nil {
 			fmt.Fprintf(w, eventsText)
+			fmt.Fprintf(w, "<a href=\"/logout\">log out </a>")
 		}
 	} else {
 
@@ -126,11 +89,22 @@ func handleMain(w http.ResponseWriter, r *http.Request) {
 }
 
 var (
-	// TODO: randomize it
 	oauthStateString = "pseudo-random"
 )
 
 var store = sessions.NewCookieStore([]byte("123"))
+
+func handleGoogleLogout(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "session.id")
+	session.Values["token"] = nil
+	var err = sessions.Save(r, w)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	url := "/"
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
 
 func handleGoogleLogin(w http.ResponseWriter, r *http.Request) {
 	googleOauthConfig.Scopes = []string{"https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/calendar.events.readonly"}
@@ -144,10 +118,9 @@ func handleGoogleCallback(w http.ResponseWriter, r *http.Request) {
 	_, err := storeToken(w, r)
 	if err != nil {
 		fmt.Println(err.Error())
-		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 		return
 	}
-	// fmt.Fprintf(w, "Content: %s\n", content)
+	http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 }
 
 func storeToken(w http.ResponseWriter, r *http.Request) (bool, error) {
@@ -197,66 +170,5 @@ func getMyEvents(token *oauth2.Token) (string, error) {
 	}
 	// contents := []byte(response)
 	return response, nil
-
-}
-
-func getUserInfo(state string, code string) ([]byte, error) {
-	if state != oauthStateString {
-		return nil, fmt.Errorf("invalid oauth state")
-	}
-	token, err := googleOauthConfig.Exchange(oauth2.NoContext, code)
-	if err != nil {
-		return nil, fmt.Errorf("code exchange failed: %s", err.Error())
-	}
-	response, err := http.Get("https://www.googleapis.com/oauth2/v2/userinfo?access_token=" + token.AccessToken)
-	if err != nil {
-		return nil, fmt.Errorf("failed getting user info: %s", err.Error())
-	}
-	defer response.Body.Close()
-	contents, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return nil, fmt.Errorf("failed reading response body: %s", err.Error())
-	}
-	return contents, nil
-}
-
-func handleGoogleLoginOLD(w http.ResponseWriter, r *http.Request) {
-
-	ctx := context.Background()
-	b, err := os.ReadFile("credentials.json")
-	if err != nil {
-		log.Fatalf("Unable to read client secret file: %v", err)
-	}
-
-	// If modifying these scopes, delete your previously saved token.json.
-	config, err := google.ConfigFromJSON(b, calendar.CalendarReadonlyScope)
-	if err != nil {
-		log.Fatalf("Unable to parse client secret file to config: %v", err)
-	}
-	client := getClient(config)
-
-	srv, err := calendar.NewService(ctx, option.WithHTTPClient(client))
-	if err != nil {
-		log.Fatalf("Unable to retrieve Calendar client: %v", err)
-	}
-
-	t := time.Now().Format(time.RFC3339)
-	events, err := srv.Events.List("primary").ShowDeleted(false).
-		SingleEvents(true).TimeMin(t).MaxResults(10).OrderBy("startTime").Do()
-	if err != nil {
-		log.Fatalf("Unable to retrieve next ten of the user's events: %v", err)
-	}
-	fmt.Println("Upcoming events:")
-	if len(events.Items) == 0 {
-		fmt.Println("No upcoming events found.")
-	} else {
-		for _, item := range events.Items {
-			date := item.Start.DateTime
-			if date == "" {
-				date = item.Start.Date
-			}
-			fmt.Printf("%v (%v)\n", item.Summary, date)
-		}
-	}
 
 }
