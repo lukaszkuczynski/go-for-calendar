0b158dca5f2c28f92d8216b21f5e0613d7d8a2c0
Lukasz Kuczynski <lukasz.kuczynski@bp.com>
2023-12-29 17:47:28 +0100

    reads and filters

--- readcalendar.go
+++ readcalendar.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 	"time"
 
 	"github.com/gorilla/sessions"
@@ -39,7 +40,7 @@ func main() {
 	http.HandleFunc("/login", handleGoogleLogin)
 	http.HandleFunc("/logout", handleGoogleLogout)
 	http.HandleFunc("/callback", handleGoogleCallback)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe("localhost:8080", nil)
 
 }
 
@@ -65,18 +66,21 @@ func handleMain(w http.ResponseWriter, r *http.Request) {
 			}
 			saveToken(r, token2, w)
 		}
-		eventsText, err := getMyEvents(token2)
+		eventsText, err := getThisMonthEventsWithTime(token2, "[mM]inistry", true)
 		if err == nil {
 			fmt.Fprintf(w, eventsText)
-			fmt.Fprintf(w, "<a href=\"/logout\">log out </a>")
+		}
+		klsEventsText, err := getThisMonthEventsWithTime(token2, "[kK][lLłŁ][sS].+", true)
+		if err == nil {
+			fmt.Fprintf(w, klsEventsText)
 		}
 	} else {
 
 		var htmlIndex = `<html>
-<body>
-	<a href="/login">Google Log In</a>
-</body>
-</html>`
+		<body>
+			<a href="/login">Google Log In</a>
+		</body>
+		</html>`
 		fmt.Fprintf(w, htmlIndex)
 	}
 
@@ -148,6 +152,56 @@ func saveToken(r *http.Request, token *oauth2.Token, w http.ResponseWriter) {
 	session, _ := store.Get(r, "session.id")
 	session.Values["token"] = token
 	session.Save(r, w)
+}
+
+func getThisMonthEventsWithTime(token *oauth2.Token, regex string, currentMonth bool) (string, error) {
+	t := time.Now()
+	monthStart := t.AddDate(0, -1, 0).Month()
+	if currentMonth {
+		monthStart = t.AddDate(0, 0, 0).Month()
+	}
+	timeMin := time.Date(t.Year(), monthStart, 0, 0, 0, 0, t.Nanosecond(), t.Location())
+	timeMaxStr := timeMin.AddDate(0, 1, 0).Format(time.RFC3339)
+	timeMinStr := timeMin.Format(time.RFC3339)
+	ctx := context.Background()
+	var src oauth2.TokenSource = googleOauthConfig.TokenSource(ctx, token)
+	srv, err := calendar.NewService(ctx, option.WithTokenSource(src))
+	if err != nil {
+		log.Fatalf("Unable to retrieve Calendar client: %v", err)
+	}
+	events, err := srv.Events.List("primary").ShowDeleted(false).
+		SingleEvents(true).TimeMin(timeMinStr).TimeMax(timeMaxStr).MaxResults(1000).OrderBy("startTime").Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve next ten of the user's events: %v", err)
+	}
+	fmt.Println("Events:")
+	var response string = ""
+	var totalDuration = time.Duration(0)
+	if len(events.Items) == 0 {
+		fmt.Println("No events found!")
+	} else {
+		for _, item := range events.Items {
+			matched, _ := regexp.Match(regex, []byte(item.Summary))
+			if matched {
+				date := item.Start.DateTime
+				if date == "" {
+					date = item.Start.Date
+				}
+				eventStart, _ := time.Parse(time.RFC3339, item.Start.DateTime)
+				eventEnd, _ := time.Parse(time.RFC3339, item.End.DateTime)
+				duration := eventEnd.Sub(eventStart)
+				totalDuration += duration
+				// fmt.Printf("%v (%v)\n", item.Summary, date)
+				eventLine := fmt.Sprintf("%v : %v (%v)\n", item.Start.DateTime, item.Summary, duration.Hours())
+				response += eventLine
+				fmt.Println(eventLine)
+			}
+		}
+	}
+	response += fmt.Sprintf("Total duration = %v", totalDuration.Hours())
+	// contents := []byte(response)
+	return response, nil
+
 }
 
 func getMyEvents(token *oauth2.Token) (string, error) {
